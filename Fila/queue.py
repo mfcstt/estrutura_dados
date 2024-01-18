e4fa7fa1c26c7bcc3b048dbbcd355ce86f92c454
Maria Fernanda Rizzo <mahcosta54@gmail.com>
2024-01-18 11:44:09 -0300

    Introduzido estrutura de pilha e fila

---
+++ Fila/queue.py
@@ -0,0 +1,67 @@
+from node import Node
+
+class Queue:
+  def __init__(self):
+    self.first = None
+    self.last = None
+    self._size = 0
+    
+  
+  def push(self, elem):
+    node = Node(elem)
+    if self.last is None:
+      self.last = node
+    else:
+      self.last.next = node  
+      self.last = node
+      
+    if self.first is None:
+      self.first = node  
+      
+    self._size = self._size + 1  
+   
+  
+  def pop(self):
+    if self.first is not None:
+      elem = self.first.data
+      self.first = self.first.next
+      self._size = self._size -1
+      return elem
+    else:
+      raise IndexError("The queue is empty")
+  
+  def peek(self):
+    if self.first is not None:
+      elem = self.first.data
+      return elem
+    raise IndexError("The queue is empty")       
+  
+  def __len__(self) :
+    return self._size
+  
+    
+  def __repr__(self):
+    if self._size > 0: 
+      r = ""
+      pointer = self.first
+      while(pointer):
+        r = r + str(pointer.data) + " "
+        pointer = pointer.next
+      return r 
+    return "Empty queue" 
+   
+    
+  def __str__(self):
+      return self.__repr__()
+    
+fila = Queue()
+
+          
+      
+
+
+
+        
+  
+
+   
