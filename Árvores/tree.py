a0815bd9f58b92c8fdb692e87b9d85ce2d2b9195
Maria Fernanda Rizzo <mahcosta54@gmail.com>
2024-01-19 12:34:53 -0300

    Implementando árvore binária

---
+++ Árvores/tree.py
@@ -0,0 +1,32 @@
+class Node:
+
+	def __init__(self, data):
+		self.data = data
+		self.left = None
+		self.right = None
+	
+	def __str__(self):
+		return str(self.data)
+	
+class BinaryTree:
+	def __init__(self, data=None):
+		if data:
+			node = Node(data)
+			self.root = node
+		else:
+			self.root = None
+   
+	def simetric_traversal(self, node=None):
+		if node is None:
+			node = self.root
+		if node.left:
+			print('(', end='')
+			self.simetric_traversal(node.left)
+		print(node, end='')
+		if node.right:
+			self.simetric_traversal(node.right)
+			print('(', end='')
+
+if __name__ == "__main__":
+  pass
+
